ff5095017e0baeaba0063cc516d1492ae4e71ba2
hasan-ilker <hasanilkerunver@gmail.com>
2023-12-23 02:12:04 +0300

    modify

---
+++ tf-files/variables.tf
@@ -0,0 +1,15 @@
+variable "git-name" {
+  default = "Hasan-ilker"
+}
+
+variable "git-token" {
+  default = "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"
+}
+
+variable "key-name" {
+  default = "firstkey"
+}
+
+variable "hosted-zone" {
+  default = "hsnawspath.click"
+}
